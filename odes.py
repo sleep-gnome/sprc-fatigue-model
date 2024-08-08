3cb8aaa0db2fe81de9a116fe24fd167b7c2a0454
sleep-gnome <mark.mccauley@wsu.edu>
2024-08-08 14:10:52 -0700

    Working Wake ODE

--- odes.py
+++ odes.py
@@ -15,3 +15,18 @@ def wakeode(t, z, theta):
     dkdt = lam * k * (1 - k)
 
     return [dpdt, dudt, dkdt]
+
+def sleepode(t, z, theta):
+    (alphaw, alphas, beta, etaw, Wc, muw, mus, lam, phi, xi) = theta
+    etas = etaw * Wc / (Wc - 24)
+    (p, u, k) = z
+
+    c = math.sin(2 * math.pi / 24 * (t - phi))
+
+    dpdt = -alphaw * (p - beta * u) + xi * k * (c + muw)
+
+    dudt = etaw * u
+
+    dkdt = lam * k * (1 - k)
+
+    return [dpdt, dudt, dkdt]
