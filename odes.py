c4843b32ccea984010c16335f9775542cc271ebd
sleep-gnome <mark.mccauley@wsu.edu>
2024-08-09 10:43:30 -0700

    All current ODEs added

--- odes.py
+++ odes.py
@@ -1,32 +1,40 @@
 import math
 
 
-def wakeode(t, z, theta):
-    (alphaw, alphas, beta, etaw, Wc, muw, mus, lam, phi, xi) = theta
+def wakeode(t, z, theta, p0):
+    (alphaw, alphas, beta, etaw, Wc, muw, mus, lam, phi, xi, nu, gam, zeta) = theta
     etas = etaw * Wc / (Wc - 24)
-    (p, u, k) = z
+    (p, u, h, k) = z
 
     c = math.sin(2 * math.pi / 24 * (t - phi))
 
-    dpdt = -alphaw * (p - beta * u) + xi * k * (c + muw)
+    dpdt = -alphaw * ((p - p0) - beta * u) + xi * k * (c + muw)
 
-    dudt = etaw * u
+    dudt = -etaw * (zeta * xi * (p - p0) - u)
+
+    dhdt = -nu * h
 
     dkdt = lam * k * (1 - k)
 
-    return [dpdt, dudt, dkdt]
+# allp = (p - p0) + xi * h
 
-def sleepode(t, z, theta):
-    (alphaw, alphas, beta, etaw, Wc, muw, mus, lam, phi, xi) = theta
+    return [dpdt, dudt, dhdt, dkdt]
+
+def sleepode(t, z, theta, p0):
+    (alphaw, alphas, beta, etaw, Wc, muw, mus, lam, phi, xi, nu, gam, zeta) = theta
     etas = etaw * Wc / (Wc - 24)
-    (p, u, k) = z
+    (p, u, h, k) = z
 
     c = math.sin(2 * math.pi / 24 * (t - phi))
 
-    dpdt = -alphas * (p - beta * (u + 1/etas)) + xi * k * (c - mus)
+    dpdt = -alphas * ((p - p0) - beta * (u + 1/etas)) + xi * k * (c - mus)
 
-    dudt = etas * u + 1
+    dudt = etas * (zeta * xi * (p - p0) - u) + 1
+
+    dhdt = nu * (gam * (p - p0) - h)
 
     dkdt = -lam * k
 
-    return [dpdt, dudt, dkdt]
+# allp = (p - p0) + xi * h
+
+    return [dpdt, dudt, dhdt, dkdt]
